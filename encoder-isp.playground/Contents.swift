9d7766cb7176ce0b9db2d12dc73f80787448ceea
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2016-01-06 10:49:21 -0500

    adding comments

--- encoder-isp.playground/Contents.swift
+++ encoder-isp.playground/Contents.swift
@@ -2,25 +2,48 @@
 
 import Cocoa
 
+//Creat an empty string to store input message
 var myMessage: String
 
-myMessage = "hello my name is carlos"
+//Input message to be decoded
+myMessage = "I am Groot"
 
+// Counts the number of characters in the input text.
 var characters: Int = myMessage.characters.count
 
+// Create empty array to store the key for each letter in
 var key: [Int] = []
 
+// Create empty array to store the ascii value for each letter in
 var asciiValue: [Int] = []
 
+// Create empty array to store the encoded in
+var encodedText: [Int] = []
+
+// Create empty array to store each letter in
 var letters: [String] = []
+
+//
+// This portion of the code encodes the text input.
+//
 
 for character in myMessage.characters
 {
-    key += [Int(arc4random_uniform(100000))]
+    
+    key += [Int(arc4random_uniform(10000000))]
     letters += [String(character)]
 }
 
-for scalar in myMessage.unicodeScalars
+for ascii in myMessage.unicodeScalars
 {
-    asciiValue += [Int(scalar.value)]
+    asciiValue += [Int(ascii.value)]
 }
+
+for (var i = 0; i < characters; i++)
+{
+    encodedText += [key[i] + asciiValue[i]]
+    
+}
+
+var test : String = String(encodedText[0])
+var aLetterCode : Int = 87
