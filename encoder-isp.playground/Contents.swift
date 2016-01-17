5b3978502ca6560c5ff05e778c5f8de115f4c7c2
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2016-01-17 09:14:52 -0500

    Commented most of the code
    decided to just spot the encryption at the 6 digit number stage because
    I could not think of a good way to change to ascii values then text due
    to randomness

--- encoder-isp.playground/Contents.swift
+++ encoder-isp.playground/Contents.swift
@@ -17,11 +17,14 @@ var key: [Int] = []
 // Create empty array to store the ascii value for each letter in
 var asciiValue: [Int] = []
 
-// Create empty array to store the encoded in
+// Create empty array to store the encoded text in
 var encodedText: [Int] = []
 
-// Create empty array to store the encoded in
-var decodedText: [Int] = []
+// Create empty array to store the decoded text in
+var decodedTextArray: [Int] = []
+
+// Create empty string to store the decoded text in
+var decodedTextString: String = ""
 
 // Create empty array to store each letter in
 var letters: [String] = []
@@ -38,35 +41,44 @@ var lowerBoudary = 99968
 // This portion of the code encodes the text input.
 //
 
+// Runs through the entire message assigning a random value to each character
 for character in myMessage.characters
 {
     randomValue = Int(arc4random_uniform(1000000))
     
+    // The random value must be within a range so that the output has 6 digits
     while randomValue < lowerBoudary || randomValue > upperBoudary
     {
+        // Must be an int to add later
         randomValue = Int(arc4random_uniform(1000000))
     }
+    
+    // Add the random value to the key array
     key += [randomValue]
+    
+    // Add each character in an array
     letters += [String(character)]
 }
 
+// Runs through the entire message finding the ascii value of each character
 for ascii in myMessage.unicodeScalars
 {
+    // Add each value to an array
     asciiValue += [Int(ascii.value)]
 }
 
+// Runs the amount of times that there are characters
 for (var i = 0; i < characters; i++)
 {
+    // Add the key and ascii value to get the 6 digit number to represent each character
     encodedText += [key[i] + asciiValue[i]]
-    
 }
 
+// Runs the amount of times that there are characters
 for (var i = 0; i < characters; i++)
 {
-    decodedText += [encodedText[i] - key[i]]
+    // decode the text by subtracting the key from the encoded text
+    decodedTextArray += [encodedText[i] - key[i]]
 }
 
-
-var test : String = String(encodedText[0])
-var aLetterCode : Int = 87
-
+String(UnicodeScalar(decodedTextArray[3]))
